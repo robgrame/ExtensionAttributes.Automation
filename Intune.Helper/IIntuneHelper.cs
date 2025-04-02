e80b673813e764a7a8a420fb3f11bd1fbf4f7764
Roberto Gramellini <roberto@gramellini.net>
2025-04-02 15:27:46 +0000

    Add Intune.Helper project and device management features
    This commit introduces a new project "Intune.Helper" targeting .NET 8.0, which includes a new interface `IIntuneHelper` for managing Intune devices. The `IntuneHelper` class implements this interface, utilizing the Microsoft Graph API to retrieve device data and log information. Several methods for fetching compliant and non-compliant devices are fully implemented, while others are defined but not yet implemented. Additionally, a `version.json` file is added for versioning with Nerdbank.GitVersioning, enhancing the overall functionality of the project.
    
    Add Intune.Helper project and device management interface
    
    This commit introduces a new project "Intune.Helper" to the solution, including the project file with necessary NuGet package references. A new interface `IIntuneHelper` is defined for managing and retrieving device information from Intune, along with its implementation in the `IntuneHelper` class. Additionally, a `version.json` file is added for version management using Nerdbank.GitVersioning.

---
+++ Intune.Helper/IIntuneHelper.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Graph.Models;
+
+namespace Azure.Automation.Intune
+{
+    public interface IIntuneHelper
+    {
+
+
+        #region Number of Devices
+        Task<ManagedDeviceCollectionResponse> GetIntuneDevices();
+        Task<int> GetTotalWindowsDevices();
+        Task<int> GetTotalMacOSDevices();
+        Task<int> GetTotaliOSDevices();
+        Task<int> GetTotalAndroidDevices();
+        Task<int> GetTotalWindows365Devices();
+        #endregion
+
+        #region Compliance
+        Task<int> GetTotalCompliantDevices();
+        Task<int> GetTotalNonCompliantDevices();
+        Task<int> GetTotalCompliantWindowsDevices();
+        Task<int> GetTotalNonCompliantWindowsDevices();
+
+        Task<ManagedDeviceCollectionResponse> GetCompliantDevices();
+        Task<ManagedDeviceCollectionResponse> GetNonCompliantDevices();
+        Task<ManagedDeviceCollectionResponse> GetCompliantWindowsDevices();
+        Task<ManagedDeviceCollectionResponse> GetNonCompliantWindowsDevices();
+
+        Task<ComplianceState> GetDeviceComplianceState(string deviceId);
+        Task<ComplianceState> GetDeviceComplianceStatebyName(string deviceName);
+
+        #endregion
+
+
+        #region Windows 365
+        #endregion
+
+
+        #region Device Details
+        Task<ManagedDevice> GetDeviceDetails(string deviceId);
+        Task<ManagedDevice> GetDeviceDetailsbyName(string deviceName);
+        #endregion
+
+
+        #region Autopilot
+        Task<bool> IsAutopilotRegistered(string serialNumber);
+        Task<bool> IsAutopilotRegistered(string serialNumber, string hardwareHash);
+        Task<string> RegisterAutoPilotDevice(string serialNumber, string hardwareHash);
+        Task<string> RegisterAutoPilotDevice(string serialNumber, string hardwareHash, string groupTag);
+        Task<string> RegisterAutoPilotDevice(string serialNumber, string hardwareHash, string groupTag, string userUPN);
+        #endregion
+    }
+}
---
+++ Intune.Helper/IntuneHelper.cs
@@ -0,0 +1,200 @@
+﻿using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace Azure.Automation.Intune
+{
+    public class IntuneHelper : IIntuneHelper
+    {
+        private readonly ILogger<IIntuneHelper> _logger;
+        private readonly GraphServiceClient _graphServiceClient;
+
+        public IntuneHelper(ILogger<IIntuneHelper> logger, GraphServiceClient graphClient)
+        {
+            _logger = logger;
+            _graphServiceClient = graphClient;
+        }
+
+        public async Task<ManagedDeviceCollectionResponse> GetIntuneDevices()
+        {
+            try
+            {
+                var managedDevices = await _graphServiceClient.DeviceManagement.ManagedDevices.GetAsync();
+                if (managedDevices?.Value == null)
+                {
+                    return new ManagedDeviceCollectionResponse();
+                }
+
+                managedDevices.Value?.ForEach(device =>
+                {
+                    _logger.LogInformation("Device {DeviceName} with {DeviceID}", device.DeviceName, device.Id);
+                });
+
+                return managedDevices;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error getting Intune devices");
+                return new ManagedDeviceCollectionResponse();
+            }
+        }
+
+        public async Task<int> GetTotalAndroidDevices()
+        {
+            _logger.LogTrace("Getting total Android devices");
+
+            var androidDevices = await _graphServiceClient.DeviceManagement.ManagedDevices.GetAsync(requestConfiguration =>
+            {
+                requestConfiguration.QueryParameters.Filter = "operatingSystem eq 'Android'";
+            });
+
+            var totalAndroidDevices = androidDevices?.Value?.Count ?? 0;
+            _logger.LogDebug("Total Android devices: {TotalAndroidDevices}", totalAndroidDevices);
+
+            return totalAndroidDevices;
+        }
+
+        public async Task<int> GetTotalCompliantDevices()
+        {
+            _logger.LogTrace("Getting total compliant devices");
+
+            var comopliantDevices = await _graphServiceClient.DeviceManagement.ManagedDevices.GetAsync(requestConfiguration =>
+            {
+                requestConfiguration.QueryParameters.Filter = "complianceState eq 'compliant'";
+            });
+
+            _logger.LogDebug("Total Compliant devices: {CompliantDevices}", comopliantDevices?.Value?.Count ?? 0);
+
+
+            return comopliantDevices?.Value?.Count ?? 0;
+        }
+
+        public async Task<int> GetTotalNonCompliantDevices()
+        {
+            _logger.LogTrace("Getting total noncompliant devices");
+
+            var comopliantDevices = await _graphServiceClient.DeviceManagement.ManagedDevices.GetAsync(requestConfiguration =>
+            {
+                requestConfiguration.QueryParameters.Filter = "complianceState eq 'noncompliant'";
+            });
+
+            _logger.LogDebug("Total NonCompliant devices: {NonCompliantDevices}", comopliantDevices?.Value?.Count ?? 0);
+
+
+            return comopliantDevices?.Value?.Count ?? 0;
+        }
+
+
+        public async Task<int> GetTotalCompliantWindowsDevices()
+        {
+            _logger.LogDebug("Getting total compliant Windows devices");
+
+            var compliantWindowsDevices = await _graphServiceClient.DeviceManagement.ManagedDevices.GetAsync(requestConfiguration =>
+            {
+                requestConfiguration.QueryParameters.Filter = "complianceState eq 'compliant' and operatingSystem eq 'Windows'";
+            });
+
+            return compliantWindowsDevices?.Value?.Count ?? 0;
+        }
+
+        public async Task<int> GetTotalNonCompliantWindowsDevices()
+        {
+            _logger.LogDebug("Getting total compliant Windows devices");
+
+            var compliantWindowsDevices = await _graphServiceClient.DeviceManagement.ManagedDevices.GetAsync(requestConfiguration =>
+            {
+                requestConfiguration.QueryParameters.Filter = "complianceState eq 'noncompliant' and operatingSystem eq 'Windows'";
+            });
+
+            return compliantWindowsDevices?.Value?.Count ?? 0;
+        }
+
+        public async Task<int> GetTotaliOSDevices()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> GetTotalMacOSDevices()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public async Task<int> GetTotalWindows365Devices()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> GetTotalWindowsDevices()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public async Task<ManagedDeviceCollectionResponse> GetCompliantDevices()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ManagedDeviceCollectionResponse> GetNonCompliantDevices()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ManagedDeviceCollectionResponse> GetCompliantWindowsDevices()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ManagedDeviceCollectionResponse> GetNonCompliantWindowsDevices()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ManagedDevice> GetDeviceDetails(string deviceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ManagedDevice> GetDeviceDetailsbyName(string deviceName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> IsAutopilotRegistered(string serialNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> IsAutopilotRegistered(string serialNumber, string hardwareHash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> RegisterAutoPilotDevice(string serialNumber, string hardwareHash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> RegisterAutoPilotDevice(string serialNumber, string hardwareHash, string groupTag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> RegisterAutoPilotDevice(string serialNumber, string hardwareHash, string groupTag, string userUPN)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ComplianceState> GetDeviceComplianceState(string deviceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ComplianceState> GetDeviceComplianceStatebyName(string deviceName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
